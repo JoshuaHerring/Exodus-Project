6a80d9368b60f09e2e4f7dc1b1ce623a5ccedd88
Joshua Day Herring <jdherring2222@gmail.com>
2025-03-31 16:16:56 -0600

    new level and moved scripts around

---
+++ scripts/level_scripts/test_level_3.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+const spawn_points : Array[Vector2] = [Vector2(0, 100), Vector2(0, -300)]
+var id: int = 3
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
